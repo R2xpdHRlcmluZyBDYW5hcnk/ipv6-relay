7251b37d3636904a6cf2227f2a57987c2cd1d9f8
R2xpdHRlcmluZyBDYW5hcnk <github@canary.sh>
2026-09-06 15:42:52 +0800

    chore: bump version to v0.1-1

--- cmd/ipv6-relay/main.go
+++ cmd/ipv6-relay/main.go
@@ -23,7 +23,7 @@ func ipv6Enabled() bool {
 	return true
 }
 
-const version = "v0.1"
+const version = "v0.1-1"
 
 func printUsage(app string, exitCode int) {
 	fmt.Fprintf(os.Stderr, "== %s Usage ==\n"+
